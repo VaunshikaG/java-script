4fa20935b7198afe4aacefa6ac089d80b61a277f
vaunshika <vgogarkar99@gmail.com>
2025-06-10 21:00:45 +0530

    IIFE done

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,1 +1,21 @@
 // Immediately Invoked Function Expression (IIFE)
+
+// ()function () execution
+// 1=> function executes immediately
+// 2=> sometimes problem is created due to pollution of global scope, so to overcome it IIFE is used
+
+(function chai() {
+  console.log("DB CONNECTED");
+})();
+// if ; is not there error will be raised
+
+// by arrow fun
+// unnamed IIFE
+(() => {
+  console.log("db connected");
+})();
+
+// named IIFE
+((name) => {
+  console.log(`db connected ${name}`);
+})("sammy");
